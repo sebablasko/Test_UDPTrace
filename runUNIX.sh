e934ce1990b009c47e7e6050671b31220488e0eb
Sebastian <sebablasko@gmail.com>
2015-08-27 13:00:09 -0300

    eliminado puerto en comando

--- runUNIX.sh
+++ runUNIX.sh
@@ -5,9 +5,9 @@ MAX_PACKS=$1
 shift 1
 amplificador=$1
 shift 1
-threads=$1
-shift 1
 num_threads=$1
+shift 1
+num_port=$1
 shift 1
 num_clients=$1
 
@@ -19,7 +19,7 @@ echo function_graph > /sys/kernel/debug/tracing/current_tracer
 
 
 # Lanzar Programa
-echo "Evaluación UDP: ./internetUDPServerTesis --packets $MAX_PACKS --threads $num_threads --port $num_port ..."
+echo "Evaluación UNIX: ./unixUDPServerTesis --packets $MAX_PACKS --threads $num_threads --verbose&"
 ./unixUDPServerTesis --packets $MAX_PACKS --threads $num_threads --verbose&
 pid=$!
 sleep 1
@@ -31,10 +31,3 @@ for ((i=1 ; $i<=$num_clients ; i++))
 
 wait $pid
 "Done"
-
-echo "Rescatando Trazas..."
-cat /sys/kernel/debug/tracing/trace > $salida"Internet.txt"
-echo "Done"
-
-# Limpiar pid
-> /sys/kernel/debug/tracing/set_ftrace_pid
